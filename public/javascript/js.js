a106bc0b19c0c716b6d80ed668180f44e2596c5b
Андрей черенецкий <you@example.com>
2019-05-03 17:24:54 +0300

    mob

--- public/javascript/js.js
+++ public/javascript/js.js
@@ -1,39 +1,47 @@
-/*
-$(function(){
+$(document).ready(function () {
 
-  $('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
-    var target = $(this).attr('href'),
-      bl_top = $(target).offset().top -70;
-    $('body, html').animate({scrollTop: bl_top}, 700);
+  $('ul.nav-main a[href^="#"]').click(function () {
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 500);
+    $('ul.nav-main a[href^="#"]').css({
+      'color': '#212121'
+    });
+    $(this).css({
+      'color': '#004bee'
+    });
     return false;
   });
 
-
-});
-*/
-
-
-
-$(document).ready(function() {
-
-  // Hidden menu
-  $( '.section, .hidden-menu__link' ).click( function() {
-    if ( $( '.hidden-menu' ).is( ':hidden' ) ) {
-      $( '.hidden-menu' ).show();
+  /* Выпадающее меню */
+  $('.menu-icon').click(function(){
+    $('.nav-main').slideToggle(500);
+    $('ul').css({
+      'display':'flex', 'flex-direction':'column'
+    })
+    if($('.menu-icon').html()=='<i class="fas fa-bars"></i>') {
+      $(this).html('<i class="fas fa-times"></i>')
     } else {
-      $( '.hidden-menu' ).hide();
+      $(this).html('<i class="fas fa-bars"></i>')
     }
   });
 
-  // Page scroll
-  $("a[rel='m_PageScroll2id']").mPageScroll2id({
-    offset:30
+  /* Кнопка наверх */
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0)
+      $('#toTop').fadeIn();
+    else
+      $('#toTop').fadeOut();
   });
+  $('#toTop').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+  });
+});
 
-
-
-
-
-
-
-
+var btn = document.querySelectorAll('button');
+for (var i =0; i<btn.length; i++) {
+  btn[i].style.outline = 'none';
+}
