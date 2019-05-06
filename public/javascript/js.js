ff7bb9d03db05801b3d00e425710d68526d67146
Андрей черенецкий <you@example.com>
2019-05-06 15:49:25 +0300

    new menu

--- public/javascript/js.js
+++ public/javascript/js.js
@@ -1,47 +1,18 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  $('ul.nav-main a[href^="#"]').click(function () {
-    var target = $(this).attr('href');
-    $('html, body').animate({
-      scrollTop: $(target).offset().top
-    }, 500);
-    $('ul.nav-main a[href^="#"]').css({
-      'color': '#212121'
-    });
-    $(this).css({
-      'color': '#004bee'
-    });
-    return false;
-  });
-
-  /* Выпадающее меню */
-  $('.menu-icon').click(function(){
-    $('.nav-main').slideToggle(500);
-    $('ul').css({
-      'display':'flex', 'flex-direction':'column'
-    })
-    if($('.menu-icon').html()=='<i class="fas fa-bars"></i>') {
-      $(this).html('<i class="fas fa-times"></i>')
+  // Hidden menu
+  $( '.menu-btn, .hidden-menu__item a' ).click( function() {
+    if ( $( '.hidden-menu' ).is( ':hidden' ) ) {
+      $( '.hidden-menu' ).show();
     } else {
-      $(this).html('<i class="fas fa-bars"></i>')
+      $( '.hidden-menu' ).hide();
     }
   });
 
-  /* Кнопка наверх */
-  $(window).scroll(function () {
-    if ($(this).scrollTop() != 0)
-      $('#toTop').fadeIn();
-    else
-      $('#toTop').fadeOut();
+  // Page scroll
+  $("a[rel='m_PageScroll2id']").mPageScroll2id({
+    offset:30
   });
-  $('#toTop').click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 800);
-  });
-});
 
-var btn = document.querySelectorAll('button');
-for (var i =0; i<btn.length; i++) {
-  btn[i].style.outline = 'none';
-}
+
+});
